f38f167a315e9063e9eaf164c9057ab0d883597b
Mike Huang <wellwind@mail2000.com.tw>
2019-09-12 14:33:43 +0800

    練習使用 Template driven form 設計表單與驗證邏輯

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { LoginFail } from '../login-user';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,20 +13,21 @@ import { LoginFail } from '../login-user';
 export class LoginComponent implements OnInit {
   email = '';
   password = '';
-
   constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {}
 
-  login() {
-    this.authService.login(this.email, this.password).subscribe(
-      result => {
-        console.log(result);
-        this.router.navigateByUrl('/');
-      },
-      (err: HttpErrorResponse ) => {
-        alert((err.error as LoginFail).body[0]);
-      }
-    );
+  login(form: NgForm) {
+    if (form.valid) {
+      this.authService.login(form.value.email, form.value.password).subscribe(
+        result => {
+          console.log(result);
+          this.router.navigateByUrl('/');
+        },
+        (err: HttpErrorResponse) => {
+          alert((err.error as LoginFail).body[0]);
+        }
+      );
+    }
   }
 }

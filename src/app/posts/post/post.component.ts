76adda0d624640a4d02bb831b6c23ab03751bc58
Mike Huang <wellwind@mail2000.com.tw>
2019-09-12 11:00:36 +0800

    任務04：在程式中取得路由參數
    順便練習 async pipe 與一些常用的 operators

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from '../post.service';
+import { Article } from '../article';
+import { map, switchMap, single, share, shareReplay } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +11,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  id: string;
+  article: Article;
+  article$: Observable<Article>;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private postService: PostService) {}
 
   ngOnInit() {
-  }
+    // this.route.paramMap.subscribe(map => {
+    //   console.log(map);
+    //   this.id = map.get('id');
 
+    //   this.postService.getArticle(this.id).subscribe(singleArticle => {
+    //     this.article = singleArticle.article;
+    //   });
+    // });
+
+    // this.route.paramMap
+    //   .pipe(
+    //     map(paramMap => paramMap.get('id')),
+    //     switchMap(id => this.postService.getArticle(id))
+    //   )
+    //   .subscribe(singleArticle => {
+    //     this.article = singleArticle.article;
+    //   });
+
+    this.article$ = this.route.paramMap.pipe(
+      map(paramMap => paramMap.get('id')),
+      switchMap(id => this.postService.getArticle(id)),
+      map(singleArticle => singleArticle.article),
+      // share()
+      shareReplay(1)
+    );
+  }
 }

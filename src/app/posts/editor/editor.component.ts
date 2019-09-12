dff512ad2307f2f98bea42a564e55abc21ef0c61
Mike Huang <wellwind@mail2000.com.tw>
2019-09-12 15:38:41 +0800

    任務09：使用 Model-driven Form 繫結表單資料

--- src/app/posts/editor/editor.component.ts
+++ src/app/posts/editor/editor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormArray } from '@angular/forms';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-editor',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editor.component.css']
 })
 export class EditorComponent implements OnInit {
+  form = this.fb.group({
+    title: this.fb.control('test'),
+    description: this.fb.control(''),
+    body: this.fb.control('body...'),
+    tagList: this.fb.array([this.fb.control('HTML'), this.fb.control('CSS'), this.fb.control('JavaSCript')])
+  });
 
-  constructor() { }
-
-  ngOnInit() {
+  get tags() {
+    return (this.form.get('tagList') as FormArray).controls;
   }
 
+  constructor(private fb: FormBuilder, private postService: PostService) {}
+
+  ngOnInit() {
+    // console.log(this.form.get('tagList').controls);
+    // console.log();
+  }
+
+  addTag() {
+    (this.form.get('tagList') as FormArray).push(this.fb.control(''));
+  }
+
+  removeTag(index: number) {
+    (this.form.get('tagList') as FormArray).removeAt(index);
+  }
+
+  save() {
+    this.form.get('description').setValue(this.form.get('body').value);
+    console.log(this.form.value);
+    this.postService.createArticle(this.form.value).subscribe(result => {
+      console.log(result);
+    });
+  }
 }
